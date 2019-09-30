6c8cdc443047ce781fc90e61ca70423ffa98c8a9
Arturo LG <lgzarturo@gmail.com>
2019-09-30 01:47:35 -0500

    Cambios en el carousel y en la página principal

--- catalogs/views.py
+++ catalogs/views.py
@@ -5,8 +5,8 @@ from projects.models import Project
 
 
 def index(request):
-    articles = Article.objects.all()
-    projects = Project.objects.all()
+    articles = Article.objects.filter(is_draft=False)[:6]
+    projects = Project.objects.all()[:6]
     context = {
         'articles': articles,
         'projects': projects,
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,7 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover();
-})
+  $('.carousel').carousel({
+    interval: 6000
+  });
+});
